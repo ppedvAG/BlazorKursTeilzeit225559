c874f764111708febb108b0954495d0a681f058e
Hannes Preishuber <hannesp@ppedv.de>
2023-02-07 12:10:59 +0100

    Parameter einer Componente aus URL

---
+++ BlazorKursTeilzeit225559/Pages/Modul2/sepp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorKursTeilzeit225559.Pages.Modul2
+{
+    public class seppModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
