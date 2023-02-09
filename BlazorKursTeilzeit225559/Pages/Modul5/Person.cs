874194052227456b63c326e3befc2bf4d8260814
Hannes Preishuber <hannesp@ppedv.de>
2023-02-09 11:29:25 +0100

    Edit Form mit Validierung per Annotationen

---
+++ BlazorKursTeilzeit225559/Pages/Modul5/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorKursTeilzeit225559.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="da muss was rein")]
+        [StringLength(10, ErrorMessage ="DB Schema max 10")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        [Range (150, 190,ErrorMessage ="zwischen 150 und 180")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "da muss was rein")]
+        public DateTime GebDat { get; set; }
+
+
+    }
+}
